300daa1537587f86cbf2ed450619b2040eb0d11a
BadBrahmin <tejasshree95@gmail.com>
2019-09-26 11:52:14 +0530

    boilerplate commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="container">
+        
+
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
