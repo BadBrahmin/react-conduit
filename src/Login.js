e6e7d2cc6727859370e6a4c6321ff04a5da10113
BadBrahmin <tejasshree95@gmail.com>
2019-09-26 12:17:37 +0530

    add login and register elements

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,20 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container flex">
-        {/* Register Elements */}
+{/* Sign In Elements */}
         {/* Top Text */}
         <div className="topText">
-          <h1>Sign Up</h1>
+          <h1>Sign In</h1>
           <h4>Need an account?</h4>
         </div>
 
         {/* input */}
         <div className="inputDiv flex">
-          <input type="text" placeholder="Username"></input>
           <input type="email" placeholder="Email"></input>
           <input type="password" placeholder="Password"></input>
         </div>
 
         {/* btn */}
         <div>
-          <button className="btn">Sign Up</button>
+          <button className="btn">Sign in</button>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default App;
