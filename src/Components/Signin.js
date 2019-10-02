79c97e5e73b817190bd7320eafba938629b7ac6d
BadBrahmin <tejasshree95@gmail.com>
2019-10-02 21:32:39 +0530

    add sign in

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -2,6 +2,35 @@ import React from "react";
 import Header from "./Header";
 
 class Signin extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("fetch");
+    console.log(this.state);
+    fetch("https://conduit.productionready.io/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ user: this.state })
+    })
+      .then(res => res.json())
+      .then(data => {
+        // localStorage.setItem("authToken", data.authToken);
+        this.props.history.push("/");
+      });
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     return (
       <>
@@ -13,14 +42,28 @@ class Signin extends React.Component {
                 <h2>Sign In</h2>
               </div>
 
-              <div className="flex-center flex-column">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-              </div>
-
-              <button className="btn" type="submit">
-                Sign In
-              </button>
+              <form
+                className="flex-center flex-column"
+                onSubmit={this.handleSubmit}
+              >
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+                <button className="btn" type="submit">
+                  Sign In
+                </button>
+              </form>
             </div>
           </div>
         </div>
